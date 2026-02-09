1aa6e063d8b6db026faa296d7ccd849178a6ce3e
Shamko Aliaksand <shamkoag@gmail.com>
2026-02-09 21:12:43 +0100

    Add verbose error logging for DB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,12 +6,16 @@ import {runDB} from "./repositories/db";
 
 const startApp = async () => {
     try {
+        console.log("Attempting to connect to DB...");
         await runDB();
+        console.log("DB connected, starting server...");
+
         app.listen(config.port, () => {
             console.log(`Server running on port ${config.port}`);
         });
     } catch (err) {
-        console.error("Failed to start server:", err);
+        console.error("FATAL ERROR DURING STARTUP:");
+        console.error(err);
         process.exit(1);
     }
 };
